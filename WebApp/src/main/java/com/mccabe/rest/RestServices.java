737c58a7783608d1e6f017efa4c1f4f024f54b8e
actuator <actuator@gmail.com>
2017-12-13 00:58:52 +0900

    1차 웹페이지

--- WebApp/src/main/java/com/mccabe/rest/RestServices.java
+++ WebApp/src/main/java/com/mccabe/rest/RestServices.java
@@ -1,14 +1,22 @@
 package com.mccabe.rest;
 
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
 
 @Path("/mccabe")
 public class RestServices {
     private static DBService dbService = new DBService();
     @POST
     @Path("/process")
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
     public String process(String json) {
+        return dbService.doProcess(json);
+    }
+
+    @POST
+    @Path("/test")
+    @Consumes(MediaType.TEXT_PLAIN)
+    public String test(String json) {
         return dbService.doProcess(json);
     }
 }
