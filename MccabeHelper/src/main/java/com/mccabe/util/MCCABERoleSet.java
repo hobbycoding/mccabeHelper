c9c83b2649c5880296b51180cdfd84386e537aca
Hyoju Choe <hjchoe@miracom.co.kr>
2018-02-07 18:38:57 +0900

    text parsing 중 Z 이상 count가 안되던 현상 수정.

--- MccabeHelper/src/main/java/com/mccabe/util/MCCABERoleSet.java
+++ MccabeHelper/src/main/java/com/mccabe/util/MCCABERoleSet.java
@@ -3,6 +3,10 @@ package com.mccabe.util;
 import com.github.javaparser.ast.body.Parameter;
 
 public class MCCABERoleSet {
+    private static int ColumnBase = 26;
+    private static int DigitMax = 7; // ceil(log26(Int32.Max))
+    private static String Digits = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
     public static String convert(Parameter param) {
         if (param.toString().contains("[]")) {
             if (param.getType().toString().equals("String"))
@@ -22,7 +26,7 @@ public class MCCABERoleSet {
             if (n.contains(",")) {
                 String result = "";
                 for (String v : n.split(",")) {
-                    result+= v.substring(v.lastIndexOf(".") + 1, v.length()) + ",";
+                    result += v.substring(v.lastIndexOf(".") + 1, v.length()) + ",";
                 }
                 return s.substring(0, s.indexOf("(") + 1).concat(result.substring(0, result.lastIndexOf(",")).concat(")"));
             } else {
@@ -33,7 +37,17 @@ public class MCCABERoleSet {
         return s;
     }
 
-    public static String getModuleLetter(int i) {
-        return "   " + (char)(i + 65);
+    public static String getModuleLetter(int value) {
+        return String.format( "%4s", getModuleLetters(value).trim());
+    }
+
+    private static String getModuleLetters(int value) {
+        char charVal = (char) ('A' + value % 26);
+        int div = value / 26;
+        if (div > 0) {
+            return getModuleLetter(div - 1) + charVal;
+        } else {
+            return "" + charVal;
+        }
     }
 }
